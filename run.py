4913ddf903b8a281da4445693a925b79f0bcc140
GuySomeDC <guysomedc@gmail.com>
2020-06-11 16:36:59 +0900

    일간 차트 / 곡 상세정보 클래스 사용 에시 추가

--- run.py
+++ run.py
@@ -48,3 +48,15 @@ for d in data:
     print(d.songname, d.artist, d.songid, d.count, d.male, d.female, d.age10, d.age20, d.age30, d.age40, d.age50, d.age60)
     print(d.getMale(), d.getFemale(), d.getAge10(), d.getAge20(), d.getAge30(), d.getAge40(), d.getAge50(), d.getAge60())
 countnum.refresh()
+
+#ex5) 일간차트
+dailychart = melonapi.Dailychart()
+datetime = dailychart.getDatetime()
+print(datetime.strftime("%Y%m%d"))
+data = dailychart.getChartdata()
+for d in data:
+    print(d.currank, d.updown, d.rankgap, d.songname, d.songid)
+    #ex6) 곡 상세정보
+    detail = melonapi.Detailinfo(d.songid)
+    detail_data = detail.getData()
+    print(detail_data.currank, detail_data.count, detail_data.male, detail_data.female, detail_data.age10, detail_data.age20, detail_data.age30, detail_data.age40, detail_data.age50, detail_data.age60)
